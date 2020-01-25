a01967851d19ba4ddab47568937d5f9a578684de
Henry Chang <henry35208@hotmail.com.tw>
2020-01-26 00:55:28 +0800

    [add] add stylelint and adjust css

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -1,10 +1,24 @@
 const { override, fixBabelImports, useEslintRc } = require('customize-cra');
+const StylelintPlugin = require('stylelint-webpack-plugin');
 
 module.exports = override(
   useEslintRc('.eslintrc.json'),
+  useStylelintRc('.stylelintrc.json'),
   fixBabelImports('import', {
     libraryName: 'antd',
     libraryDirectory: 'es',
     style: 'css',
   })
 );
+
+function useStylelintRc(configFile) {
+  return (config, env) => {
+    config.plugins.push(
+      new StylelintPlugin({
+        configFile,
+        files: ['**/*.css', '**/*.sass', '**/*.scss', '**/*.less'],
+      })
+    );
+    return config;
+  };
+}
