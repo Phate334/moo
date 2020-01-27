75dae42a7c18162250b07f3477801ba6a59b492a
Henry Chang <henry35208@hotmail.com.tw>
2020-01-28 00:16:18 +0800

    [add] add storybook [refactor] modify folder structure

--- client/src/pages/app/App.tsx
+++ client/src/pages/app/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
 import { Button } from 'antd';
 import './App.css';
+import logo from '../../assets/logo.svg';
 
 const App: React.FC = () => {
   return (
